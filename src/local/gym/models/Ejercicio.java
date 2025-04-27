2a601d9a9e4768d12717a3be36fca0dec657b802
JUANGUTIERREZ <gutierrezjuan3020@gmail.com>
2025-04-27 15:10:48 -0500

    ritina y ejecicis

--- src/local/gym/models/Ejercicio.java
+++ src/local/gym/models/Ejercicio.java
@@ -1,1 +1,56 @@
-// gutierrez
+package models;
+
+public class Ejercicio {
+    private String nombre;
+    private String descripcion;
+    private int repeticiones;
+    private int series;
+
+    // Constructor
+    public Ejercicio(String nombre, String descripcion, int repeticiones, int series) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.repeticiones = repeticiones;
+        this.series = series;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    // Método para mostrar información del ejercicio
+    @Override
+    public String toString() {
+        return "Ejercicio: " + nombre + ", " + descripcion + ", " +
+               repeticiones + " repeticiones x " + series + " series.";
+    }
+}
--- src/local/gym/models/Rutina.java
+++ src/local/gym/models/Rutina.java
@@ -1,1 +1,44 @@
-// gutierrez
+package local.gym.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rutina {
+    private String nombreRutina;
+    private List<Ejercicio> ejercicios;
+
+    // Constructor
+    public Rutina(String nombreRutina) {
+        this.nombreRutina = nombreRutina;
+        this.ejercicios = new ArrayList<>();
+    }
+
+    // Método para agregar ejercicios a la rutina
+    public void agregarEjercicio(Ejercicio ejercicio) {
+        ejercicios.add(ejercicio);
+    }
+
+    // Getters y Setters
+    public String getNombreRutina() {
+        return nombreRutina;
+    }
+
+    public void setNombreRutina(String nombreRutina) {
+        this.nombreRutina = nombreRutina;
+    }
+
+    public List<Ejercicio> getEjercicios() {
+        return ejercicios;
+    }
+
+    // Método para mostrar la rutina completa
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Rutina: ").append(nombreRutina).append("\n");
+        for (Ejercicio ejercicio : ejercicios) {
+            sb.append("- ").append(ejercicio.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
