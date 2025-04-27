569f494946557710d5fb8c184230b5b7a0bf6d0b
Juan Simancas <dev.juansimancas@proton.me>
2025-04-27 13:50:50 -0500

    cambios a cliente, contratista y entrenador

---
+++ src/local/gym/models/Contratista.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.util.List;
+
+public class Contratista extends Persona {
+    private List<Maquina> maquinasAsignadas;
+    private Turno turnoAtencion = Turno.MANANA;
+
+    public Contratista(int id, String nombre, String telefono, String cedula, String email,
+            List<Maquina> maquinasAsignadas, Turno turnoAtencion) {
+        super(id, nombre, telefono, cedula, email);
+        this.maquinasAsignadas = maquinasAsignadas;
+        this.turnoAtencion = turnoAtencion;
+    }
+
+    public List<Maquina> getMaquinasAsignadas() {
+        return maquinasAsignadas;
+    }
+
+    public void setMaquinasAsignadas(List<Maquina> maquinasAsignadas) {
+        this.maquinasAsignadas = maquinasAsignadas;
+    }
+
+    public Turno getTurnoAtencion() {
+        return turnoAtencion;
+    }
+
+    public void setTurnoAtencion(Turno turnoAtencion) {
+        this.turnoAtencion = turnoAtencion;
+    }
+
+}
